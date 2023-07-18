75b8ee1f29abce12099eb5ad18d315d6afe4ceb0
Kerry Guarino <kerry.guarino@gmail.com>
2023-07-17 21:14:50 -0400

    click on stock creates console.log

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Stock({name, ticker, type, price}) {
+
+  const [isBought, setIsBought] = useState(false)
+
+  function buyStock(){
+    console.log("you bought: ", name)
+    //set state of isBought to True...
+    //send back up to Main Container
+    //filter True items to new array
+    //set
+
+
+  }
+
+
   return (
     <div>
-      <div className="card">
+      <div className="card" onClick={buyStock}>
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
           <p className="card-text">{price}</p>
